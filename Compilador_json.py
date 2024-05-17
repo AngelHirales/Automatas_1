21ee52eebc4fee0de34e0c4e34fbec80b9872f4d
AngelHirales <115297427+AngelHirales@users.noreply.github.com>
2024-05-16 21:03:35 -0700

    Update#3 Compilador_json.py
    cosas que ya funcionan:
    - lee el contenido del archivo json
    - imprime los caracteres separados en consola y las palabras dentro de comillas con el token 999
    - muestra la salida tambien en un archivo de texto
    - muestra el contenido del array con los tokens de todos los caracteres
    
    cambios recientes:
    - se mejoro la logica de algunas funciones optimizando el codigo
    - se agrego una funcion para trabajar con fechas (aun falta pulir algunos detalles como que le asigne un token especial a las fechas)

--- Compilador_json.py
+++ Compilador_json.py
@@ -39,70 +39,93 @@ def tokenizar_palabra(file):
     en_comillas = False
 
     for char in file:
-        # Caracter de comillas dobles
         if char == chr(34):
             if en_comillas:
-                # Fin de palabra y asignar token 999
                 lista_tokens[palabra] = 999
                 palabra = ''
                 en_comillas = False
             else:
-                # Inicio de palabra
                 en_comillas = True
         elif en_comillas:
-            # Agregar caracter a la palabra
             palabra += char
         else:
-            # Manejar caracteres no dentro de comillas
             value = ord(char)
             lista_tokens[char] = value
 
-    # Agregar la última palabra si no está vacía
     if palabra:
         lista_tokens[palabra] = 999
 
     array_tokens = list(lista_tokens.values())
-    return lista_tokens, array_tokens
+    return lista_tokens, array_tokens 
+
+
+# comprueba si se cerraron las comillas
+def comillas_cerradas(array):
+    bandera = False
+    for valor in array:
+        if valor == 34:
+            bandera = not bandera
+    if bandera:
+        print("\n❌!!ERROR: Las comillas no se abrieron/cerraron correctamente!!❌")
+        exit(1)
+    else:
+        print('\n✅Codigo JSON leido y ejecutado correctamente✅')
+        exit(1)
+
+
+# Función para tokenizar fechas en formato DD/MM/AAAA
+def tokenizar_fecha(array):
+    tokens = []
+    date_token = ''
+    is_date = False
+    for value in array:
+        if 47 <= value <= 57:
+            date_token += chr(value)
+            if date_token.count('/') == 2 and len(date_token) == 10:
+                tokens.append(203)
+                date_token = ''
+                is_date = True
+        else:
+            if is_date:
+                is_date = False
+            tokens.append(value)
+    return tokens
+
 
 # imprime el contenido del archivo y sus tokens correspondientes
 def imprimir_tokens(file, tokens):
     lista_tokens = {}
     palabra = ''
     en_comillas = False
+    consecutive_spaces = 0
 
     for char in file:
-        # Caracter de comillas dobles
         if char == chr(34):
             if en_comillas:
-                # Fin de palabra y asignar token 999
                 lista_tokens[palabra] = 999
-                print(f'999="{palabra}"')  # Imprimir palabra agrupada con comillas y token
+                print(f'999="{palabra}"')
                 palabra = ''
                 en_comillas = False
             else:
-                # Inicio de palabra
                 en_comillas = True
-
         elif en_comillas:
-        # Agregar caracter a la palabra
             palabra += char
-
-        elif char == chr(10):
+        elif char == '\n':
             print(f'{tokens[char]}=[ENTER]')
-        elif char == chr(32):
-            print(f'{tokens[char]}=[ESPACIO]')
+        elif char == ' ':
+            consecutive_spaces += 1
+            if consecutive_spaces == 4:
+                print(f'{tokens[char]}=[TAB]')
+                consecutive_spaces = 0
         elif char in tokens:
             print(f'{tokens[char]}={char}')
-
         else:
-        # Manejar caracteres no dentro de comillas
             value = ord(char)
             lista_tokens[char] = value
 
-    # Agregar la última palabra si no está vacía
     if palabra:
         lista_tokens[palabra] = 999
-        print(f'999="{palabra}"')  # Imprimir palabra agrupada con comillas y token
+        print(f'999="{palabra}"')
 
     array_tokens = list(lista_tokens.values())
     return lista_tokens, array_tokens
@@ -110,72 +133,43 @@ def imprimir_tokens(file, tokens):
 
 # Escribir un archivo output.txt los tokens y caracteres
 def escribir_archivo(file, tokens):
-
     with open('output.txt', 'w') as output:
         lista_tokens = {}
         palabra = ''
         en_comillas = False
 
         for char in file:
-            # Caracter de comillas dobles
             if char == chr(34):
                 if en_comillas:
-                    # Fin de palabra y asignar token 999
                     lista_tokens[palabra] = 999
-                    output.write(f'999="{palabra}"\n')  # Imprimir palabra agrupada con comillas y token
+                    output.write(f'999="{palabra}"\n')
                     palabra = ''
                     en_comillas = False
                 else:
-                    # Inicio de palabra
                     en_comillas = True
-
             elif en_comillas:
-                # Agregar caracter a la palabra
                 palabra += char
-
-            elif char == chr(10):
-                # Salto de línea
+            elif char == '\n':
                 output.write(f'{tokens[char]}=[ENTER]\n')
-
-            elif char == chr(32):
-                # Espacio en blanco
+            elif char == ' ':
                 output.write(f'{tokens[char]}=[ESPACIO]\n')
-
             elif char in tokens:
-                # Caracter normal
                 output.write(f'{tokens[char]}={char}\n')
-
             else:
-                # Manejar caracteres no dentro de comillas
                 value = ord(char)
                 lista_tokens[char] = value
-                # Escribir el caracter directamente
                 output.write(char)
 
-        # Agregar la última palabra si no está vacía
         if palabra:
             lista_tokens[palabra] = 999
             output.write(f'999="{palabra}"\n')
 
         array_tokens = list(lista_tokens.values())
-        return output,lista_tokens,array_tokens
-
-
-# comprueba si se cerraron las comillas
-def comillas_cerradas(array):
-    bandera = True
-    if bandera == True :
-        print('\n✅Codigo JSON leido y ejecutado correctamente✅')
-        exit(1)
-    for valor in array:
-        if valor == 34:
-            bandera = not bandera
-    if bandera == False:
-        print("\n❌!!ERROR: Las comillas no se abrieron/cerraron correctamente!!❌")
-        exit(1)
+        return output, lista_tokens, array_tokens
 
 
 def main():
+
     print('-----------------------------------------------------------------------------------------------------------------------------')
     print('CONTENIDO DEL ARCHHIVO JSON: ')
     print('-----------------------------------------------------------------------------------------------------------------------------')
@@ -183,21 +177,19 @@ def main():
     # variable file para manejar el contenido del archivo
     file = leer_json('json_example.json')
     tokens = tokenizar(file)
-    
-    # mostrar el contenido del archivo
-    imprimir_tokens(file, tokens)
 
-    # generar el archivo output.txt
+    # If quotes are balanced, continue with processing
+    imprimir_tokens(file, tokens)
     escribir_archivo(file, tokens)
-    
-    print('----------------------------------------------------------------------------------------------------------------------------')
+
+    print('-----------------------------------------------------------------------------------------------------------------------------')
     print('ARRAY DE TOKENS: ')
     print('-----------------------------------------------------------------------------------------------------------------------------')
-   
+
     # Imprime el array de los tokens
     print(array_tokens)
-    
-    # Imprime un error en caso de que la comilla no este cerrada
+
+    # si las comillas no estan cerradas muestra el error
     print(comillas_cerradas(array_tokens))
 
 if __name__ :" __main__"
